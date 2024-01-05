6cebb612d319fe97df11f5ae475beba151839eb1
vilsonmoro <vilsonmoro@gmail.com>
2024-01-05 08:13:31 -0300

    Adicionar arquivos de projeto.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Models
+{
+    public class TodoContext : DbContext
+    {
+       /* public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }*/
+
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+        
+    }
+}
